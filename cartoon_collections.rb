106b766952694ffcbcc0a488b40daf2ce17c4d5c
Michael Jester <m.jester.93@gmail.com>
2020-05-22 23:58:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |x|
-    puts "Hello ${x}!"
+    puts "Hello #{x}!"
   end
 end
 
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |x, index|
-    puts "${index}. ${x}"
+    puts "#{index}. #{x}"
   end
 end
